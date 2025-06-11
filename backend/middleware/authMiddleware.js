d21cd36258e12e6f68268de3ad9bc36aa4a881e4
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2025-06-11 16:55:40 -0300

    Adição do script 'start' para deploy do front no Render

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,22 +2,22 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = function (req, res, next) {
-    // Obter o token do cabeçalho
-    const token = req.header('x-auth-token');
+  // Obter o token do cabeçalho
+  const token = req.header('x-auth-token');
 
-    // Verifica se não há token
-    if (!token) {
-        return res.status(401).json({ msg: 'Nenhum token, autorização negada.'});
-    }
+  // Verifica se não há token
+  if (!token) {
+    return res.status(401).json({ msg: 'Nenhum token, autorização negada.'});
+  }
 
-    try {
-        // Verifica o token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  try {
+    // Verifica o token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        // Anexar o objeto de usuário decodificado à requisição
-        req.user = decoded.user;
-        next(); // Prosseguir para a próxima função middleware/rota
-    } catch (err) {
-        res.status(401).json({ msg: 'Token não é válido. '});
-    }
+    // Anexar o objeto de usuário decodificado à requisição
+    req.user = decoded.user;
+    next(); // Prosseguir para a próxima função middleware/rota
+  } catch (err) {
+    res.status(401).json({ msg: 'Token não é válido. '});
+  }
 };
